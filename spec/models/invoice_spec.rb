27de0ee59ebaa3b0aaf78ca2c0480461164ebc51
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-27 12:10:44 -0600

    Add model test for discounted revenue method

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -23,5 +23,23 @@ RSpec.describe Invoice, type: :model do
 
       expect(@invoice_1.total_revenue).to eq(100)
     end
+
+    describe "#discounted_revenue" do
+      before(:each) do
+        @merchant_1 = Merchant.create!(name: 'Hair Care')
+        @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant_1.id, status: 1)
+        @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant_1.id)
+        @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+        @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+        @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
+        @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+        @bulk_discount_1 = @merchant_1.bulk_discounts.create!(discount_percent: 20, threshold: 10)
+        @bulk_discount_2 = @merchant_1.bulk_discounts.create!(discount_percent: 10, threshold: 5)
+      end
+
+      it "returns the revenue from an invoice after discounts (if they apply)" do
+        expect(@invoice_1.discounted_revenue).to eq(0)
+      end
+    end
   end
 end
