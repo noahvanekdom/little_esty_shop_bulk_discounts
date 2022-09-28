50f2f6e9990f40fefc176a3b5f0c9b1aad982244
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-27 22:42:25 -0600

    I hate activerecord

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,10 +7,20 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :bulk_discounts, through: :merchants
 
   enum status: [:cancelled, 'in progress', :complete]
 
   def total_revenue
     invoice_items.sum("unit_price * quantity")
   end
+
+  def discounted_revenue
+    Invoice.from(invoice_items.joins(item:[merchant: :bulk_discounts])
+    .where("invoice_items.quantity >= bulk_discounts.threshold")
+    .select('invoice_items.id, max(invoice_items.quantity * invoice_items.unit_price * (bulk_discounts.discount_percent)/100) as discount')
+    .group('invoice_items.id'))
+    .sum('discount')
+  end
+
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,9 +7,11 @@ RSpec.describe Invoice, type: :model do
   end
   describe "relationships" do
     it { should belong_to :customer }
+    it { should have_many(:invoice_items) }
     it { should have_many(:items).through(:invoice_items) }
     it { should have_many(:merchants).through(:items) }
-    it { should have_many :transactions}
+    it { should have_many(:bulk_discounts).through(:merchants) }
+    it { should have_many :transactions }
   end
   describe "instance methods" do
     it "total_revenue" do
@@ -27,18 +29,40 @@ RSpec.describe Invoice, type: :model do
     describe "#discounted_revenue" do
       before(:each) do
         @merchant_1 = Merchant.create!(name: 'Hair Care')
+        @merchant_2 = Merchant.create!(name: 'Sporty Spice')
+
         @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant_1.id, status: 1)
+        @item_2 = Item.create!(name: "Hair Tie", description: "Too cool for school", unit_price: 2, merchant_id: @merchant_1.id)
+        @item_3 = Item.create!(name: "Dye", description: "Colors", unit_price: 50, merchant_id: @merchant_1.id)
+        @item_4 = Item.create!(name: "Football", description: "Ball for foot", unit_price: 15, merchant_id: @merchant_2.id)
         @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant_1.id)
+
         @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+        @customer_2 = Customer.create!(first_name: 'Mark', last_name: 'Johnson')
+        @customer_3 = Customer.create!(first_name: 'Sam', last_name: 'Cal')
+        @customer_4 = Customer.create!(first_name: 'Mitch', last_name: 'Black')
+
         @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
-        @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 9, unit_price: 10, status: 2)
-        @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 1, unit_price: 10, status: 1)
+        @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-29 14:54:09")
+        @invoice_3 = Invoice.create!(customer_id: @customer_2.id, status: 2, created_at: "2012-03-20 14:54:09")
+        @invoice_4 = Invoice.create!(customer_id: @customer_3.id, status: 2, created_at: "2012-03-30 14:54:09")
+
+
+        @invoice_item_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 19, unit_price: 10, status: 2)
+        @invoice_item_2 =  InvoiceItem.create!(invoice_id: @invoice_2.id, item_id: @item_2.id, quantity: 20, unit_price: 15, status: 2)
+        @invoice_item_3 = InvoiceItem.create!(invoice_id: @invoice_3.id, item_id: @item_3.id, quantity: 1, unit_price: 10, status: 1)
+        invoice_item_4 = InvoiceItem.create!(invoice_id: @invoice_4.id, item_id: @item_3.id, quantity: 100, unit_price: 50, status: 2)
+        @invoice_item_4 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 22, unit_price: 10, status: 1)
+
+
         @bulk_discount_1 = @merchant_1.bulk_discounts.create!(discount_percent: 20, threshold: 10)
         @bulk_discount_2 = @merchant_1.bulk_discounts.create!(discount_percent: 10, threshold: 5)
       end
 
       it "returns the revenue from an invoice after discounts (if they apply)" do
-        expect(@invoice_1.discounted_revenue).to eq(0)
+        expect(@invoice_1.discounted_revenue).to eq(82.0)
+        expect(@invoice_2.discounted_revenue).to eq(60.0)
+        expect(@invoice_3.discounted_revenue).to eq(0.0)
       end
     end
   end
