d48d49ba70f63b324b62c5caca6b121ca20cfbe0
Noah van Ekdom <noahvanekdom@gmail.com>
2022-09-28 04:36:08 -0600

    Add feature and model tests for finding best applicable discount

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -16,6 +16,8 @@ RSpec.describe InvoiceItem, type: :model do
   describe "class methods" do
     before(:each) do
       @m1 = Merchant.create!(name: 'Merchant 1')
+      @m2 = Merchant.create!(name: 'Merchant 2')
+      @m3 = Merchant.create!(name: 'Merchant 3')
       @c1 = Customer.create!(first_name: 'Bilbo', last_name: 'Baggins')
       @c2 = Customer.create!(first_name: 'Frodo', last_name: 'Baggins')
       @c3 = Customer.create!(first_name: 'Samwise', last_name: 'Gamgee')
@@ -25,18 +27,25 @@ RSpec.describe InvoiceItem, type: :model do
       @item_1 = Item.create!(name: 'Shampoo', description: 'This washes your hair', unit_price: 10, merchant_id: @m1.id)
       @item_2 = Item.create!(name: 'Conditioner', description: 'This makes your hair shiny', unit_price: 8, merchant_id: @m1.id)
       @item_3 = Item.create!(name: 'Brush', description: 'This takes out tangles', unit_price: 5, merchant_id: @m1.id)
+      @item_4 = Item.create!(name: 'Sword', description: 'Slishy Slashy', unit_price: 20, merchant_id: @m3.id)
       @i1 = Invoice.create!(customer_id: @c1.id, status: 2)
       @i2 = Invoice.create!(customer_id: @c1.id, status: 2)
       @i3 = Invoice.create!(customer_id: @c2.id, status: 2)
       @i4 = Invoice.create!(customer_id: @c3.id, status: 2)
       @i5 = Invoice.create!(customer_id: @c4.id, status: 2)
+      @i6 = Invoice.create!(customer_id: @c5.id, status: 2)
       @ii_1 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_1.id, quantity: 100, unit_price: 10, status: 0)
       @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 1, unit_price: 8, status: 0)
-      @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
+      @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_3.id, quantity: 400, unit_price: 5, status: 2)
       @ii_4 = InvoiceItem.create!(invoice_id: @i3.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 1)
+      @ii_5 = InvoiceItem.create!(invoice_id: @i6.id, item_id: @item_4.id, quantity: 1, unit_price: 10, status: 2)
 
-      @bulk_discount_1 = @merchant_1.bulk_discounts.create!(discount_percent: 20, threshold: 10)
-      @bulk_discount_2 = @merchant_1.bulk_discounts.create!(discount_percent: 10, threshold: 5)
+
+
+      @bulk_discount_1 = @m1.bulk_discounts.create!(discount_percent: 20, threshold: 10)
+      @bulk_discount_2 = @m1.bulk_discounts.create!(discount_percent: 10, threshold: 5)
+      @bulk_discount_4 = @m1.bulk_discounts.create!(discount_percent: 60, threshold: 500)
+      @bulk_discount_3 = @m2.bulk_discounts.create!(discount_percent: 30, threshold: 40)
     end
 
     it 'incomplete_invoices' do
@@ -45,8 +54,9 @@ RSpec.describe InvoiceItem, type: :model do
 
     describe "#find_discount" do
       it "returns the relevant discount for an invoice item" do
-        expect(@ii_1.find_discount).to eq(@bulk_discount_2)
-        expect(@ii_3.find_discount).to eq("No Discount")
+        expect(@ii_1.find_discount).to eq(@bulk_discount_1)
+        expect(@ii_3.find_discount).to eq(@bulk_discount_1)
+        expect(@ii_5.find_discount).to eq(nil)
       end
     end
   end
